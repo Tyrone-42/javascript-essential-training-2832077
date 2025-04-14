400370e1a6fa5f51fab95944bc9177150596e707
Tyrone-42 <22073582@student.westernsydney.edu.au>
2025-04-14 12:10:25 +1000

    Update script.js

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,25 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const car1 = {
+  name:"Kia Cerato",
+  color:"grey",
+  wheelsNum: 4,
+  volume: 30; 
+}
+
+const car2 = {
+  name:"Toyata Camry",
+  color: "white",
+  wheelsNum: 4,
+  volume: 25,
+  engine: {
+    engineType: "V4",
+    horsepower: 444
+  }
+}
+
+console.log(car2);
+
+
